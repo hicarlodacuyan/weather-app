f6293ab96ced5989cc6cae135d3778bfc8845a0d
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-06-30 14:46:36 +0800

    Add single h1 for testing and update scripts

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+const tailwindcss = require('tailwindcss');
+
 module.exports = {
     plugins: {
       'postcss-import': {},
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./main.scss";
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello, World!';
+h1.classList.add('text-3xl', 'font-bold', 'underline', 'text-gray-500', 'm-20', 'bg-red-900');
+
+document.getElementById('root').appendChild(h1);
