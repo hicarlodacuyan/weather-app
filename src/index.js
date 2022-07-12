0ed30b11d81e7a92ae52a169dbd3a95db2cb6580
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-07-12 16:12:26 +0800

    Add current location weather data and weather icon

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import './main.scss'
 
+const container = document.createElement('div')
+container.classList.add('flex', 'justify-center', 'items-center', 'm-20')
+
 const h1 = document.createElement('h1')
+h1.classList.add('text-3xl', 'font-bold', 'text-gray-500')
+
+const img = document.createElement('img')
 
 const findCurrentWeatherData = async loc => {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${loc},uk&APPID=e72cbe506351290ff39725a41bce3680`
+      `http://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=e72cbe506351290ff39725a41bce3680`
     )
     const currentWeatherData = await response.json()
 
-    console.log(currentWeatherData.weather[0].description)
-    h1.textContent = `Right now in London, it's ${currentWeatherData.weather[0].description}`
+    console.log(currentWeatherData.name)
+    h1.textContent = `Right now in ${currentWeatherData.name}, it's ${currentWeatherData.weather[0].description}`
+    img.src = `http://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png`
   } catch (err) {
     console.log(err)
   }
 }
 
-findCurrentWeatherData('london')
+findCurrentWeatherData('manila')
 
-h1.textContent = 'Hello, World!'
-h1.classList.add('text-3xl', 'font-bold', 'text-gray-500', 'm-20')
-
-document.getElementById('root').appendChild(h1)
+container.appendChild(h1)
+container.appendChild(img)
+document.getElementById('root').appendChild(container)
