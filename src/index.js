eb0b8111e5da2fca45a382dfeca621d02a31eeda
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2022-07-12 16:29:57 +0800

    Add resource over HTTPS

--- src/index.js
+++ src/index.js
@@ -19,13 +19,13 @@ const img = document.createElement('img')
 const findCurrentWeatherData = async loc => {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=e72cbe506351290ff39725a41bce3680`
+      `https://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=e72cbe506351290ff39725a41bce3680`
     )
     const currentWeatherData = await response.json()
 
     console.log(currentWeatherData.name)
     h1.textContent = `Right now in ${currentWeatherData.name}, it's ${currentWeatherData.weather[0].description}`
-    img.src = `http://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png`
+    img.src = `https://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png`
   } catch (err) {
     console.log(err)
   }
